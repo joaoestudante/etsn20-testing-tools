c27981e69b0680668a48d9d7e8ba364677c2cd85
João Lourenço <estudante.lourenco@gmail.com>
2020-12-02 16:10:05 +0100

    Add uuid return to login
    By returning the ID along with the token, requests
    become much easier to manage.

--- softwaretesting/urls.py
+++ softwaretesting/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 from django.views.generic.base import RedirectView
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken import views
-from .users.views import UserViewSet, UserCreateViewSet, ArticleViewSet
+from .users.views import UserViewSet, UserCreateViewSet, ArticleViewSet, ObtainAuthTokenAndUUID
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
@@ -31,7 +31,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('api/v1/', include(router.urls)),
-    path('api-token-auth/', views.obtain_auth_token),
+    path('api-token-auth/', ObtainAuthTokenAndUUID.as_view()),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
     # the 'api-root' from django rest-frameworks default router
--- softwaretesting/users/views.py
+++ softwaretesting/users/views.py
@@ -5,7 +5,8 @@ from .permissions import IsUserOrReadOnly, IsArticleOrReadOnly
 from .serializers import CreateUserSerializer, UserSerializer, ArticleSerializer
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK
-
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
 
 class UserViewSet(mixins.RetrieveModelMixin,
                   mixins.UpdateModelMixin,
@@ -44,3 +45,11 @@ class ArticleViewSet(mixins.RetrieveModelMixin,
     def update(self, request, *args, **kwargs):
         return Response(data=request.data, status=HTTP_200_OK)
 
+
+class ObtainAuthTokenAndUUID(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key, 'uuid':user.id})
